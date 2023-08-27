c71c6d28c3a606178eff4a4a5f1c96b156bc14af
Pruthviraj Chauhan <142862198+Pruthviraj36@users.noreply.github.com>
2023-08-27 16:19:46 +0530

    Add files via upload
    Signed-off-by: Pruthviraj Chauhan <142862198+Pruthviraj36@users.noreply.github.com>

---
+++ bitwise_multiplication.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+		
+		int x,y,i,mul=0;
+		printf("Enter x : ");
+		scanf("%d",&x);
+		printf("Enter y : ");
+		scanf("%d",&y);
+		
+		for(i=1;i<=y;i++){
+			mul=mul+x;
+			}
+			printf("Multiplication is :%d",mul);
+	}
---
+++ odd_even_with_operator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+		
+		int num;
+		printf("Enter number");
+		scanf("%d",&num);
+		if((num & 1)==0){
+			printf("Even");
+			}
+			else{
+				printf("Odd");
+								}
+	}
