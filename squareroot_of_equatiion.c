3e156ea4ba6e12e315529a345ab3a04c17fc87ba
Pruthviraj Chauhan <chauhanpruthviraj309@gmail.com>
2023-09-07 17:13:20 +0530

    Update squareroot_of_equatiion.c
    Signed-off-by: Pruthviraj Chauhan <chauhanpruthviraj309@gmail.com>

--- squareroot_of_equatiion.c
+++ squareroot_of_equatiion.c
@@ -1,17 +1,1 @@
-Compiling single file...
---------
-- Filename: C:\C programs\squareroot_of_equatiion.c
-- Compiler Name: TDM-GCC 4.9.2 64-bit Release
 
-Processing C source file...
---------
-- C Compiler: C:\Program Files (x86)\Dev-Cpp\MinGW64\bin\gcc.exe
-- Command: gcc.exe "C:\C programs\squareroot_of_equatiion.c" -o "C:\C programs\squareroot_of_equatiion.exe"  -I"C:\Program Files (x86)\Dev-Cpp\MinGW64\include" -I"C:\Program Files (x86)\Dev-Cpp\MinGW64\x86_64-w64-mingw32\include" -I"C:\Program Files (x86)\Dev-Cpp\MinGW64\lib\gcc\x86_64-w64-mingw32\4.9.2\include" -L"C:\Program Files (x86)\Dev-Cpp\MinGW64\lib" -L"C:\Program Files (x86)\Dev-Cpp\MinGW64\x86_64-w64-mingw32\lib" -static-libgcc
-
-Compilation results...
---------
-- Errors: 0
-- Warnings: 0
-- Output Filename: C:\C programs\squareroot_of_equatiion.exe
-- Output Size: 133.064453125 KiB
-- Compilation Time: 0.16s
