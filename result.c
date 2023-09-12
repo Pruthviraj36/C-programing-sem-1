933d0769d04fbf3989c86f6eeb11da3cd2d5d08b
Pruthviraj Chauhan <chauhanpruthviraj309@gmail.com>
2023-09-12 08:13:38 +0530

    Update result.c
    Signed-off-by: Pruthviraj Chauhan <chauhanpruthviraj309@gmail.com>

--- result.c
+++ result.c
@@ -5,32 +5,31 @@ void main()
 	int rono;
 	char Name[20];
 	float math, sci, eng, total, per, sanskrit, hindi, sst, msc, computer;
-	clrscr();
 	printf("\t\t\t\t\t Results\n\n");
-	printf("\t Enter the roll no :");
+	printf("\t Enter the roll no	:");
 	scanf("%d", &rono);
-	printf("\n\t Name :");
+	printf("\t Name			:");
 	scanf("%s", &Name);
-	printf("\n\t Enter the Maths marks :");
+	printf("\t Enter the Maths marks	:");
 	scanf("%f", &math);
-	printf("\n\t Enter the Sci mark :");
+	printf("\t Enter the Sci mark	:");
 	scanf("%f", &sci);
-	printf("\n\t Enter the Eng mark :");
+	printf("\t Enter the Eng mark	:");
 	scanf("%f", &eng);
-	printf("\n\t Enter the SST marks :");
+	printf("\t Enter the SST marks	:");
 	scanf("%f", &sst);
-	printf("\n\t Enter the Sanskrit marks :");
+	printf("\t Enter the Sans marks	:");
 	scanf("%f", &sanskrit);
-	printf("\n\t Enter the Hindi marks :");
+	printf("\t Enter the Hindi marks	:");
 	scanf("%f", &hindi);
-	printf("\n\t Enter the Computer marks :");
+	printf("\t Enter the Comp marks	:");
 	scanf("%f", &computer);
-	printf("\n\t Enter the MSC marks :");
+	printf("\t Enter the MSC marks	:");
 	scanf("%f", &msc);
 	total = math + sci + eng + sst + hindi + sanskrit + computer + msc;
-	printf(" \n\t total Is=%f\n", total);
+	printf("\t total Is=%f\n", total);
 	per = total / 8;
-	printf("\n\t Percantage is = %f", per);
+	printf("\t Percantage is = %f", per);
 	if (per < 33)
 	{
 		printf("\n\t Fail");
@@ -47,17 +46,17 @@ void main()
 	{
 		printf("\n\t Grade B");
 	}
+	else if (per < 80 && per > 70)
+	{
+		printf("\n\t Grade B+");
+	}
 	else if (per < 90 && per > 80)
 	{
 		printf("\n\t Grade A");
 	}
-	else if (per < 99 && per > 90)
+	else if (per < 100 && per > 90)
 	{
 		printf("\n\t Grade A+");
-	}
-	else
-	{
-		printf("\n\n\t Dist");
 	}
 	if (math < 35 || sci < 35 || eng < 35 || sst < 35 || hindi < 35 || computer < 35 || sanskrit < 35 || msc < 35 )
 	{
