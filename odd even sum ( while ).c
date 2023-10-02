5bfacba4a00bfd040c1bc8e4e029a4e687ea24a6
Pruthviraj Chauhan <chauhanpruthviraj309@gmail.com>
2023-10-02 11:08:09 +0530

    Update odd even sum ( while ).c
    Signed-off-by: Pruthviraj Chauhan <chauhanpruthviraj309@gmail.com>

--- odd even sum ( while ).c
+++ odd even sum ( while ).c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 void main(){
 
-	int n,i=0,evencount=0,oddcount=0;
+	int n,i=1,evencount=0,oddcount=0;
 	while(i<=10){
 		scanf("%d",&n);
 		if(n%2==0){
-		evencount++;
+			evencount=evencount+n;
 		}
-		oddcount++;
+		else{
+			oddcount=oddcount+n;
+		}
 			i++;
 	}
 	printf("Evensum is	:%d\n",evencount);
